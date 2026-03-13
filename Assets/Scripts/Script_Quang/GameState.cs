a1b9931118418217a06021314df87a69dba7d5b6
bquang016 <buidangquangmtp@gmail.com>
2026-03-13 18:01:31 +0700

    done - save/load

---
+++ Assets/Scripts/Script_Quang/GameState.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameState
+{
+    public int highScore;
+    public int currentCoin;
+    public List<string> unlockedSkins = new List<string>();
+}
+
+[System.Serializable]
+public class FruitSaveData
+{
+    public string fruitID;
+    public float posX;
+    public float posY;
+    public float rotZ;
+
+    // Constructor tiện ích
+    public FruitSaveData(string id, Vector2 pos, float rot)
+    {
+        fruitID = id;
+        posX = pos.x;
+        posY = pos.y;
+        rotZ = rot;
+    }
+}
+
+[System.Serializable]
+public class SaveData
+{
+    public GameState metaData = new GameState();
+    public List<FruitSaveData> sessionData = new List<FruitSaveData>();
+}
---
+++ Assets/Scripts/Script_Quang/MockFruitItem.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+// Gắn cái này vào các Prefab trái cây giả (VD: Hình tròn màu đỏ, hình tròn màu xanh)
+public class MockFruitItem : MonoBehaviour
+{
+    public string fruitID; // Tí nữa ra Inspector gõ "Apple", "Melon"... vào đây
+}
