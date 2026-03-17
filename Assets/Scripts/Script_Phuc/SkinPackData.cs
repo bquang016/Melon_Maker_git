ec7ad974cc23385819deb47b0e082cbf0bafb5da
phucngohihi <phuccpzz12345@gmail.com>
2026-03-17 17:31:52 +0700

    dang lam

---
+++ Assets/Scripts/Script_Phuc/SkinPackData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Skin Pack", menuName = "Suika Game/Skin Pack")]
+public class SkinPackData : ScriptableObject
+{
+    [Header("Thông tin Gói Skin")]
+    public string packID;          // Mã ID (Ví dụ: "default", "animal", "planet")
+    public string packName;        // Tên hiển thị trong Shop (Ví dụ: "Bộ Gốc")
+    public int price;              // Giá tiền để mua gói này
+    public bool isUnlocked;        // Gói này có được mở sẵn từ đầu không?
+
+    [Header("Danh sách Hình ảnh (Kéo 11 ảnh từ nhỏ đến lớn vào đây)")]
+    // Mảng này sẽ chứa 11 hình ảnh tương ứng với 11 trái cây của bộ skin này
+    public Sprite[] fruitSprites;
+}
