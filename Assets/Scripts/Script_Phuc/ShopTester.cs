bbc911e5d23c18a9dddcd60afcf456f55914bbdb
phucngohihi <phuccpzz12345@gmail.com>
2026-03-24 17:04:35 +0700

    all_skin

---
+++ Assets/Scripts/Script_Phuc/ShopTester.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class ShopTester : MonoBehaviour
+{
+    // Kéo file Skin_Animal vào ô này trên Inspector để test mua thử nó
+    public SkinPackData packToTest;
+
+    void Update()
+    {
+        // Bấm phím B để giả lập hành động user bấm nút Mua
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            ShopManager.Instance.BuySkin(packToTest);
+        }
+
+        // Bấm phím M để ăn gian thêm 500 tiền
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            ShopManager.Instance.AddMoney(500);
+            Debug.Log("Tiền hiện tại: " + ShopManager.Instance.GetCurrentMoney());
+        }
+
+        // Bấm phím R để Reset toàn bộ dữ liệu (Xóa hết tiền và skin đã mua)
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            PlayerPrefs.DeleteAll();
+            Debug.Log("Đã xóa sạch Data test!");
+        }
+    }
+}
